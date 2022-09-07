70e9992314ab8a5aff657356383ca83bcb6c1fae
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-09-07 23:27:54 +0530

    Create FEVER.cpp

---
+++ Starter_55/FEVER.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	     int x;
+	     cin>>x;
+	     
+	     if(x>98){
+	         cout<<"YES"<<endl;
+	     }
+	     else{
+	         cout<<"NO"<<endl;
+	     }
+	}
+	return 0;
+}
