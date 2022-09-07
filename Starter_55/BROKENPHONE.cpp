cc69e2200b1cde14e7c62663601a0298eca705b9
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-09-07 23:28:43 +0530

    Create BROKENPHONE.cpp

---
+++ Starter_55/BROKENPHONE.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	     int x,y;
+	     cin>>x>>y;
+	     
+	     if(x<y){
+	         cout<<"REPAIR"<<endl;
+	     }
+	     else if(x==y){
+	         cout<<"ANY"<<endl;
+	     }
+	     else{
+	         cout<<"NEW PHONE"<<endl;
+	     }
+	}
+	return 0;
+}
