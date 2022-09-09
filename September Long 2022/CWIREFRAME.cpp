0526fd83050d01374655840d5f81387bc61f23c8
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-09-09 23:53:56 +0530

    Create CWIREFRAME.cpp

---
+++ September Long 2022/CWIREFRAME.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop for(ll i=0;i<n;i++)
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n,m,x;
+	    cin>>n>>m>>x;
+	    cout<<(2*(n+m))*x<<endl;
+	}
+	return 0;
+}
