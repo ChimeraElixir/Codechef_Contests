6710d0b26994d3dae0e43e212b6305b539822044
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-09-09 23:54:25 +0530

    Create BOBBANK.cpp

---
+++ September Long 2022/BOBBANK.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop for(ll i=0;i<n;i++)
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	   int w,x,y,z;
+	   cin>>w>>x>>y>>z;
+	   cout<<w+((x-y)*z)<<endl;
+	   
+	}
+	return 0;
+}
