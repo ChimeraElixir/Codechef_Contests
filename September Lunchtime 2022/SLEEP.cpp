f70d3b4cb0a596d8467ee206fc7d2cc83a8bf9b4
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-09-23 23:38:21 +0530

    Create SLEEP.cpp

---
+++ September Lunchtime 2022/SLEEP.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop for(ll i=0;i<n;i++)
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	   int x;
+	   cin>>x;
+	   
+	   if(x>=7){
+	       cout<<"No"<<endl;
+	   }
+	   else{
+	       cout<<"Yes"<<endl;
+	   }
+	   
+	}
+	return 0;
+}
