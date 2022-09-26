267458a0a82c1a7d7e108ead3b6922e0001dabfa
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-09-27 01:01:24 +0530

    Create STICKBREAK.cpp

---
+++ Codechef_Contest_Practice/September Lunch Time 2022/STICKBREAK.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop for(ll i=0;i<n;i++)
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	   int k,l;
+	   cin>>k>>l;
+	   if(k%l==0){
+	       cout<<'0'<<endl;
+	   }
+	   else{
+	       cout<<'1'<<endl;
+	   }
+	   
+	}
+	return 0;
+}
