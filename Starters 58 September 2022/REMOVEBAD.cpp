191bdcb2d82e0a133c2bdfee15c080812f9277c2
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-09-28 23:19:40 +0530

    Create REMOVEBAD.cpp

---
+++ Starters 58 September 2022/REMOVEBAD.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop for(ll i=0;i<n;i++)
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	   int n;
+	   cin>>n;
+	   
+	   map <int,int> map;
+	   
+	   loop{
+	       int x;
+	       cin>>x;
+	       map[x]++;
+	   }
+	   std::map<int, int>::iterator itr;
+	   int x=map.begin()->second;
+	   
+	   for (itr = map.begin(); itr != map.end(); ++itr){
+            
+            if(x<itr->second){
+                x=itr->second;
+            }
+            
+        }
+        cout<<n-x<<endl;
+	}
+	
+	return 0;
+}
