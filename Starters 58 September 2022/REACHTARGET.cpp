556d5b884abf92504fbca30a8f6561b6e0cc41ca
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-09-28 23:18:10 +0530

    Create REACHTARGET.cpp

---
+++ Starters 58 September 2022/REACHTARGET.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop for(ll i=0;i<n;i++)
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	
+	    cout<<x-y<<endl;
+	   
+	}
+	return 0;
+}
