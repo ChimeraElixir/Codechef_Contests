38deaa204281bc948efc2205ce1d1b2a3e07ee66
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-10-12 23:03:34 +0530

    Create CUTPAPER.cpp

---
+++ Starter_60/CUTPAPER.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int k,n;
+	    cin>>k>>n;
+	    
+	    cout<<((k/n)*(k/n))<<endl;
+	}
+	return 0;
+}
