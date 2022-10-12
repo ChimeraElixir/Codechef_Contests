33c94fbb046464adf5dd1a62fc399964329f14db
Gaurav Verma <93048788+ChimeraElixir@users.noreply.github.com>
2022-10-12 23:02:11 +0530

    Create TAXES.cpp

---
+++ Starter_60/TAXES.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop for(ll i=0;i<n;i++)
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	   int x;
+	   cin>>x;
+	   if(x>100){
+	       cout<<x-10<<endl;
+	   }
+	   else{
+	       cout<<x<<endl;
+	   }
+	   
+	}
+	return 0;
+}
